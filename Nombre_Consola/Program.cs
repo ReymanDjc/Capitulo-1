d9b53b80823c1762e1fe93f15d04ef1610830256
ReymanDJC <ReymanDJC@REYMANDJC-PC>
2017-01-14 16:25:52 -0400

    pa riba

--- Nombre_Consola/Program.cs
+++ Nombre_Consola/Program.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
-namespace Nombre_Consola
+namespace ErrorEnCodigo
 {
     class Program
     {
         static void Main(string[] args)
         {
+            console.writeline("Edimar Tienes Un Error ejercicio 2");
         }
     }
 }
+
